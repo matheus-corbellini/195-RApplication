bc5632de341d03a02bb81b29286a0c203f5ace02
Matheus Corbellini <matheussteindorff@gmail.com>
2025-07-02 10:30:03 -0300

    tipando usuario e atualizandno o contexto

--- src/contexts/AuthContextDef.ts
+++ src/contexts/AuthContextDef.ts
@@ -7,6 +7,7 @@ interface AuthContextType {
   login: (email: string, password: string) => Promise<UserCredential>;
   signup: (email: string, password: string) => Promise<UserCredential>;
   logout: () => Promise<void>;
+  userData: import("../type/user").User | null;
 }
 
 export const AuthContext = createContext<AuthContextType | undefined>(
